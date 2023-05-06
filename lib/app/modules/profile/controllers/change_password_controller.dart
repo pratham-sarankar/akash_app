0748219d4bdf92d8cb6264e46109ad8308ac5b0b
pratham_srnk <pratham@appointy.com>
2023-05-06 15:01:38 +0530

    Corrected Change Password Feature.

--- lib/app/modules/profile/controllers/change_password_controller.dart
+++ lib/app/modules/profile/controllers/change_password_controller.dart
@@ -1,11 +1,28 @@
+import 'package:akash/app/data/repositories/profile_repository.dart';
+import 'package:akash/app/data/services/auth_service.dart';
+import 'package:akash/app/data/services/toast_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class ChangePasswordController extends GetxController {
-  //TODO: Implement ChangePasswordController
+  late GlobalKey<FormState> formKey;
+  late RxBool isLoading;
+  String? accessToken;
 
-  final count = 0.obs;
+  String? oldPassword;
+  String? newPassword;
+  String? confirmPassword;
+
+  late RxBool isConfirmPasswordHidden;
+  late RxBool isPasswordHidden;
+
   @override
   void onInit() {
+    formKey = GlobalKey<FormState>();
+    isLoading = false.obs;
+    accessToken = Get.find<AuthService>().accessToken;
+    isConfirmPasswordHidden = true.obs;
+    isPasswordHidden = true.obs;
     super.onInit();
   }
 
@@ -19,5 +36,27 @@ class ChangePasswordController extends GetxController {
     super.onClose();
   }
 
-  void increment() => count.value++;
+  void changePassword() async {
+    try {
+      if (formKey.currentState!.validate()) {
+        formKey.currentState!.save();
+        isLoading.value = true;
+        if (accessToken == null || newPassword == null) {
+          //TODO: Show formatted error for this case
+          return;
+        }
+        final repository = Get.find<ProfileRepository>();
+        var result = await repository.updatePassword(accessToken: accessToken!,password: oldPassword!,newPassword:newPassword!);
+        isLoading.value = false;
+        if (result) {
+          Get.find<ToastService>().showSuccessMessage("Password updated successfully");
+        }
+      }
+    } catch (e) {
+      isLoading.value = false;
+      Get.find<ToastService>().showErrorMessage(e.toString());
+      rethrow;
+    }
+  }
+
 }
--- lib/app/modules/profile/views/change_password_view.dart
+++ lib/app/modules/profile/views/change_password_view.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:iconly/iconly.dart';
 
@@ -12,30 +10,129 @@ class ChangePasswordView extends GetView<ChangePasswordController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ChangePassword'),
+        title: const Text('Change Password'),
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
         child: Form(
+          key: controller.formKey,
           child: Column(
             children: [
               TextFormField(
                 decoration: const InputDecoration(
                   labelText: "Old Password",
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'This field is required';
+                  }
+                  if (value.length < 8) {
+                    return 'Password must be at least 8 characters long';
+                  }
+                  final hasUppercase = value.contains(RegExp(r'[A-Z]'));
+                  if (!hasUppercase) {
+                    return 'Password must contain at least one uppercase letter';
+                  }
+                  final hasSpecialCharacters =
+                      value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+                  if (!hasSpecialCharacters) {
+                    return 'Password must contain at least one special character';
+                  }
+                  return null;
+                },
                 autofocus: true,
+                onSaved: (newValue) {
+                  controller.oldPassword = newValue;
+                },
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "New Password",
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Obx(
+                  () => TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'This field is required';
+                      }
+                      if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      final hasUppercase = value.contains(RegExp(r'[A-Z]'));
+                      if (!hasUppercase) {
+                        return 'Password must contain at least one uppercase letter';
+                      }
+                      final hasSpecialCharacters =
+                          value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+                      if (!hasSpecialCharacters) {
+                        return 'Password must contain at least one special character';
+                      }
+                      return null;
+                    },
+                    obscureText: controller.isPasswordHidden.value,
+                    onSaved: (newValue) {
+                      controller.newPassword = newValue;
+                    },
+                    onChanged: (value) {
+                      controller.newPassword = value;
+                    },
+                    decoration: InputDecoration(
+                      labelText: "New Password",
+                      hintText: "* * * * *",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          controller.isPasswordHidden.toggle();
+                        },
+                        child: Obx(
+                          () => Icon(
+                            controller.isPasswordHidden.value
+                                ? IconlyLight.hide
+                                : IconlyLight.show,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "Confirm Password",
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Obx(
+                  () => TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'This field is required';
+                      }
+                      if (value != controller.newPassword) {
+                        return 'Passwords do not match';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      controller.confirmPassword = newValue;
+                    },
+                    onChanged: (value) {
+                      controller.confirmPassword = value;
+                    },
+                    obscureText: controller.isConfirmPasswordHidden.value,
+                    decoration: InputDecoration(
+                      labelText: "Confirm Password",
+                      hintText: "* * * * *",
+                      suffixIcon: GestureDetector(
+                        onTap: () {
+                          controller.isConfirmPasswordHidden.toggle();
+                        },
+                        child: Obx(
+                          () => Icon(
+                            controller.isConfirmPasswordHidden.value
+                                ? IconlyLight.hide
+                                : IconlyLight.show,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ],
@@ -43,7 +140,7 @@ class ChangePasswordView extends GetView<ChangePasswordController> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: controller.changePassword,
         child: const Icon(Icons.check),
       ),
     );
